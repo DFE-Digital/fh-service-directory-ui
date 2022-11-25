7e5e5e8c84585d7ca6edde498ec9d9b5ce886dfe
Phil Davies <github@phildavies.co.uk>
2022-11-25 15:13:57 +0000

    tidy up

--- src/FamilyHubs.ServiceDirectory.Infrastructure/Services/PostcodesIoResponse.cs
+++ src/FamilyHubs.ServiceDirectory.Infrastructure/Services/PostcodesIoResponse.cs
@@ -3,8 +3,7 @@ using FamilyHubs.ServiceDirectory.Core.Postcode.Model;
 
 namespace FamilyHubs.ServiceDirectory.Infrastructure.Services;
 
-//todo: can we flatten the result during deserialization
-//todo: belongs in core??
+//todo: can we flatten the result during deserialization?
 public sealed record PostcodesIoResponse(
     //todo: do we need status from the body - should always match the http code?
     //[property: JsonPropertyName("status")] int Status,
--- src/FamilyHubs.ServiceDirectory.Web/Pages/ServiceFilter.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Web/Pages/ServiceFilter.cshtml.cs
@@ -1,13 +1,8 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FamilyHubs.ServiceDirectory.Web.Pages
 {
     public class ServiceFilterModel : PageModel
     {
-        public void OnGet()
-        {
-            //todo
-        }
     }
 }
