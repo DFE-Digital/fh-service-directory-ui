53b0584fe62e20cdc8da6b5a7953031a5d53ce7c
Phil Davies <github@phildavies.co.uk>
2022-11-25 12:46:26 +0000

    add placeholder filter page

---
+++ src/FamilyHubs.ServiceDirectory.Web/Pages/ServiceFilter.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FamilyHubs.ServiceDirectory.Web.Pages
+{
+    public class ServiceFilterModel : PageModel
+    {
+        public void OnGet()
+        {
+            //todo
+        }
+    }
+}
