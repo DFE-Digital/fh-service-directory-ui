6403787516b733b8aa667f8fb7f5b99be19dd329
Phil Davies <github@phildavies.co.uk>
2022-11-25 12:44:22 +0000

    postcodes wip

--- src/FamilyHubs.ServiceDirectory.Web/Pages/PostcodeSearch.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Web/Pages/PostcodeSearch.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using FamilyHubs.ServiceDirectory.Infrastructure.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 //todo: temp
 #pragma warning disable
@@ -8,6 +9,13 @@ namespace FamilyHubs.ServiceDirectory.Web.Pages
 {
     public class PostcodeSearchModel : PageModel
     {
+        public enum ErrorType
+        {
+            None,
+            Postcode
+        };
+
+        public ErrorType Error { get; set; }
         //todo: clean architecture says don't access infrastructure code in web (apart from program/startup)
         // so create a wrapper in core project
         private readonly IPostcodesIoClient _postcodesIoClient;
@@ -17,9 +25,22 @@ namespace FamilyHubs.ServiceDirectory.Web.Pages
             _postcodesIoClient = postcodesIoClient;
         }
 
-        public async Task OnPost(string postcode)
+        //todo: use post-redirect-get or not??
+        public void OnGet(ErrorType error)
         {
+            Error = error;
+        }
+
+        public async Task<IActionResult> OnPost(string? postcode)
+        {
+            if (postcode == null)
+            {
+                return RedirectToPage("/PostcodeSearch", new {error = ErrorType.Postcode});
+            }
+
             var postcodeInfo = await _postcodesIoClient.Get(postcode);
+
+            return RedirectToPage("/ServiceFilter");
         }
     }
 }
