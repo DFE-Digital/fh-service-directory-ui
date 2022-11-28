58aec0c38354061a5892076192be2f95c71f0dbe
Phil Davies <github@phildavies.co.uk>
2022-11-28 16:17:27 +0000

    filters wip

--- src/FamilyHubs.ServiceDirectory.Web/Models/FilterAspect.cs
+++ src/FamilyHubs.ServiceDirectory.Web/Models/FilterAspect.cs
@@ -5,21 +5,3 @@ namespace FamilyHubs.ServiceDirectory.Web.Models;
 
 [DebuggerDisplay("{Id}")]
 public record FilterAspect(string Id, string Description, bool Selected = false) : IFilterAspect;
-
-//todo: temp
-#pragma warning disable
-
-//[DebuggerDisplay("{Id}")]
-//public class FilterAspect : IFilterAspect
-//{
-//    public string Id { get; }
-//    public string Description { get; }
-//    public bool Selected { get; }
-
-//    public FilterAspect(string id, string description, bool selected = false)
-//    {
-//        Id = id;
-//        Description = description;
-//        Selected = selected;
-//    }
-//}
--- src/FamilyHubs.ServiceDirectory.Web/Pages/ServiceFilter.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Web/Pages/ServiceFilter.cshtml.cs
@@ -13,6 +13,11 @@ namespace FamilyHubs.ServiceDirectory.Web.Pages
             {
                 new FilterAspect("cost--free", "Free"),
                 new FilterAspect("cost--pay-to-use", "Pay to use")
+            }),
+            new Filter("show", "Show", new IFilterAspect[]
+            {
+                new FilterAspect("show--family-hubs", "Family hubs"),
+                new FilterAspect("show--services-and-groups", "Services and groups")
             })
         };
 
