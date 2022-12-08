df30acd9395c383e181cc1889e0bc56258d39160
Phil Davies <github@phildavies.co.uk>
2022-12-08 10:49:04 +0000

    category filtering wip

---
+++ src/FamilyHubs.ServiceDirectory.Web/Models/PostFilterSubGroups.cs
@@ -0,0 +1,24 @@
+﻿using FamilyHubs.ServiceDirectory.Web.Models.Interfaces;
+
+namespace FamilyHubs.ServiceDirectory.Web.Models;
+
+public interface IFilterSubGroups
+{
+    string Name { get; }
+    string Description { get; }
+    IEnumerable<IFilter> SubFilters { get; }
+}
+
+public class PostFilterSubGroups : IFilterSubGroups
+{
+    private readonly FilterSubGroups _filterSubGroups;
+
+    public PostFilterSubGroups(FilterSubGroups filterSubGroups)
+    {
+        _filterSubGroups = filterSubGroups;
+    }
+
+    public string Name => _filterSubGroups.Name;
+    public string Description => _filterSubGroups.Description;
+    public IEnumerable<IFilter> SubFilters => _filterSubGroups.SubFilters;
+}
