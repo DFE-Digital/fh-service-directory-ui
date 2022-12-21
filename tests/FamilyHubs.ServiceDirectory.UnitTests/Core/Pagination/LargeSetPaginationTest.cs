c2017b3cec2f763a86f20193b53d3fa862ac51e0
Phil Davies <github@phildavies.co.uk>
2022-12-21 09:01:33 +0000

    pagination tests

--- tests/FamilyHubs.ServiceDirectory.UnitTests/Core/Pagination/LargeSetPaginationTest.cs
+++ tests/FamilyHubs.ServiceDirectory.UnitTests/Core/Pagination/LargeSetPaginationTest.cs
@@ -11,27 +11,28 @@ public class PaginationItemTests
         var pagination = new LargeSetPagination(count, current);
         var actual = pagination.PaginationItems;
 
+        //todo: test that ignores current, and separate test to check current
         actual.Should().BeEquivalentTo(expected);
     }
 
-    //todo: add current flag to pagination item
     public static IEnumerable<object[]> Data =>
         new List<object[]>
         {
-            new object[] {0, 1, GenerateExpected()},
-            new object[] {1, 1, GenerateExpected(1)},
-            new object[] {2, 1, GenerateExpected(1, 2)},
-            new object[] {2, 2, GenerateExpected(1, 2)},
-            new object[] {3, 1, GenerateExpected(1, 2, 3)},
-            new object[] {3, 2, GenerateExpected(1, 2, 3)},
-            new object[] {3, 3, GenerateExpected(1, 2, 3)},
-            new object[] {4, 1, GenerateExpected(1, 2, null, 4)},
-            new object[] {7, 4, GenerateExpected(1, null, 3, 4, 5, null, 7)},
-            new object[] {42, 20, GenerateExpected(1, null, 19, 20, 21, null, 42)}
+            //todo: test to check don't show instead
+            //new object[] {0, 1, GenerateExpected()},
+            //new object[] {1, 1, GenerateExpected(1)},
+            new object[] {2, 1, GenerateExpected(0, 1, 2)},
+            new object[] {2, 2, GenerateExpected(1, 1, 2)},
+            new object[] {3, 1, GenerateExpected(0, 1, 2, 3)},
+            new object[] {3, 2, GenerateExpected(1, 1, 2, 3)},
+            new object[] {3, 3, GenerateExpected(2, 1, 2, 3)},
+            new object[] {4, 1, GenerateExpected(0, 1, 2, null, 4)},
+            new object[] {7, 4, GenerateExpected(3, 1, null, 3, 4, 5, null, 7)},
+            new object[] {42, 20, GenerateExpected(3, 1, null, 19, 20, 21, null, 42)}
         };
 
-    public static IEnumerable<PaginationItem> GenerateExpected(params int?[] pages)
+    public static PaginationItem[] GenerateExpected(int currentIndex, params int?[] pages)
     {
-        return pages.Select(p => p == null ? new PaginationItem() : new PaginationItem(p.Value, false));
+        return pages.Select((p, i) => p == null ? new PaginationItem() : new PaginationItem(p.Value, i == currentIndex)).ToArray();
     }
 }
