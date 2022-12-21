84b0ef1c714fa66801bb2ac12082a4ce235271f2
Phil Davies <github@phildavies.co.uk>
2022-12-21 14:15:06 +0000

    ga wip

--- src/FamilyHubs.ServiceDirectory.Web/wwwroot/js/components/analytics.js
+++ src/FamilyHubs.ServiceDirectory.Web/wwwroot/js/components/analytics.js
@@ -1,13 +1,20 @@
-//todo: GA_TODO incorporate nonce support to this from fts
+// it doesn't make sense to do typescript type checking in a javascript file where you can't specify the type (is there a way?)
+// (f is a HTMLScriptElement but we can't cast it in js)
+// so switch off javascript compilation for this file...
+// @ts-nocheck
 export default function loadAnalytics() {
-    // no-op until we add ga later
     //@* https://dev.to/matijamrkaic/using-google-tag-manager-with-a-content-security-policy-9ai*@
     //<script id="gtmScript" data-nonce="@Context.GetNonce()" asp-add-nonce>
-    //    (function (w, d, s, l, i){w[l] = w[l] || [];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
-    //    var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;
-    //    j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;var n=d.querySelector('[nonce]');n&&j.setAttribute('nonce',n.nonce||n.getAttribute('nonce'));
-    //      f.parentNode.insertBefore(j,f);})(window, document, 'script', 'dataLayer', 'GTM-PLHRMLT');
-    //</script>
+    (function (w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+        var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        var n = d.querySelector('[nonce]');
+        n && j.setAttribute('nonce', n.nonce || n.getAttribute('nonce'));
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'G-30G6ZFTEJE');
     //if (!window.ga || !window.ga.loaded) {
     //    // Load gtm script
     //    // Script based on snippet at https://developers.google.com/tag-manager/quickstart
