358df7aee9ab1a38235db80b290978a0123f5d63
Phil Davies <github@phildavies.co.uk>
2022-12-21 11:43:54 +0000

    tidy up

--- src/FamilyHubs.ServiceDirectory.Web/Pages/Error/Index.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Web/Pages/Error/Index.cshtml.cs
@@ -4,5 +4,4 @@ namespace FamilyHubs.ServiceDirectory.Web.Pages.Error;
 
 public class ErrorModel : PageModel
 {
-    //todo: check exception gets logged by middlewear, or do we need to do it ourselves?
 }
