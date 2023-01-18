dfe31ca52d588bcf8a90c134fb6d23ec8f54e841
Phil Davies <github@phildavies.co.uk>
2023-01-18 14:59:07 +0000

    switch typescript compilation from nuget to npm (in anticipation of bundling and minification)

--- src/FamilyHubs.ServiceDirectory.Web/gulpfile.js
+++ src/FamilyHubs.ServiceDirectory.Web/gulpfile.js
@@ -3,7 +3,9 @@
 var gulp = require("gulp"),
     sass = require('gulp-sass')(require('sass')),
     sourcemaps = require('gulp-sourcemaps'),
-    csso = require('gulp-csso');
+    csso = require('gulp-csso'),
+    //concat = require('gulp-concat')
+    ts = require("gulp-typescript");
 
 gulp.task('sass-to-min-css', async function () {
     return gulp.src('./styles/scss/application.scss')
@@ -16,4 +18,31 @@ gulp.task('sass-to-min-css', async function () {
 
 gulp.task('sass-to-min-css:watch', function () {
     gulp.watch('./wwwroot/scss/application.scss', gulp.series('sass-to-min-css'));
+});
+
+//gulp.task('scripts', function () {
+//    return gulp.src('./scripts/*.js')
+//        .pipe(concat('all.js'))
+//        .pipe(gulp.dest('./dist/'));
+//});
+
+
+// https://www.meziantou.net/compiling-typescript-using-gulp-in-visual-studio.htm
+
+var tsProject;
+
+gulp.task("js", function () {
+
+    if (!tsProject) {
+        tsProject = ts.createProject("tsconfig.json");
+    }
+
+    var reporter = ts.reporter.fullReporter();
+    var tsResult = tsProject.src()
+        .pipe(sourcemaps.init())
+        .pipe(tsProject(reporter));
+
+    return tsResult.js
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("./wwwroot/js"));
 });
