ea7c265d32f230799fd2ec1abacc10b443c75dab
Phil Davies <github@phildavies.co.uk>
2023-01-18 14:14:03 +0000

    minify css

--- src/FamilyHubs.ServiceDirectory.Web/gulpfile.js
+++ src/FamilyHubs.ServiceDirectory.Web/gulpfile.js
@@ -2,16 +2,18 @@
 
 var gulp = require("gulp"),
     sass = require('gulp-sass')(require('sass')),
-    sourcemaps = require('gulp-sourcemaps');
+    sourcemaps = require('gulp-sourcemaps'),
+    csso = require('gulp-csso');
 
-gulp.task('sass-to-css', async function () {
+gulp.task('sass-to-min-css', async function () {
     return gulp.src('./styles/scss/application.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(csso())
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./wwwroot/css'));
 });
 
-gulp.task('sass-to-css:watch', function () {
-    gulp.watch('./wwwroot/scss/application.scss', gulp.series('sass-to-css'));
+gulp.task('sass-to-min-css:watch', function () {
+    gulp.watch('./wwwroot/scss/application.scss', gulp.series('sass-to-min-css'));
 });
