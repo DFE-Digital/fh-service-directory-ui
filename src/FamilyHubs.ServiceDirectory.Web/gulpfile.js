874c9c3e06fe1191acc2119aa52affa4a155ce43
Phil Davies <github@phildavies.co.uk>
2023-01-18 12:18:45 +0000

    add css source map (is it right?)

--- src/FamilyHubs.ServiceDirectory.Web/gulpfile.js
+++ src/FamilyHubs.ServiceDirectory.Web/gulpfile.js
@@ -1,11 +1,14 @@
 ﻿"use strict";
 
 var gulp = require("gulp"),
-    sass = require('gulp-sass')(require('sass'));
+    sass = require('gulp-sass')(require('sass')),
+    sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass-to-css', async function () {
     return gulp.src('./styles/scss/application.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./wwwroot/css'));
 });
 
