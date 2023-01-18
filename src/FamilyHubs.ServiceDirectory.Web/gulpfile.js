a692efcc236a047f5700f575f61e3834e7a477ca
Phil Davies <github@phildavies.co.uk>
2023-01-18 12:12:07 +0000

    global gulp for transpiling, bundling, minification etc (todo css map)

---
+++ src/FamilyHubs.ServiceDirectory.Web/gulpfile.js
@@ -0,0 +1,14 @@
+﻿"use strict";
+
+var gulp = require("gulp"),
+    sass = require('gulp-sass')(require('sass'));
+
+gulp.task('sass-to-css', async function () {
+    return gulp.src('./styles/scss/application.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./wwwroot/css'));
+});
+
+gulp.task('sass-to-css:watch', function () {
+    gulp.watch('./wwwroot/scss/application.scss', gulp.series('sass-to-css'));
+});
