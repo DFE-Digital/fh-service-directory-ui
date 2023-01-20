90ceaf18b068405d7b0ed879145e95a94f317946
Phil Davies <github@phildavies.co.uk>
2023-01-20 12:15:52 +0000

    wip

--- src/FamilyHubs.ServiceDirectory.Web/Pages/ServiceFilter/Index.cshtml.cs
+++ src/FamilyHubs.ServiceDirectory.Web/Pages/ServiceFilter/Index.cshtml.cs
@@ -2,6 +2,8 @@ using System.Diagnostics.CodeAnalysis;
 using FamilyHubs.ServiceDirectory.Core.Distance;
 using FamilyHubs.ServiceDirectory.Core.Pagination;
 using FamilyHubs.ServiceDirectory.Core.Pagination.Interfaces;
+using FamilyHubs.ServiceDirectory.Core.Postcode.Interfaces;
+using FamilyHubs.ServiceDirectory.Core.Postcode.Model;
 using FamilyHubs.ServiceDirectory.Core.ServiceDirectory.Interfaces;
 using FamilyHubs.ServiceDirectory.Web.Content;
 using FamilyHubs.ServiceDirectory.Web.Filtering.Interfaces;
@@ -11,6 +13,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FamilyHubs.ServiceDirectory.Web.Pages.ServiceFilter;
+#pragma warning disable
 
 public class ServiceFilterModel : PageModel
 {
@@ -25,11 +28,13 @@ public class ServiceFilterModel : PageModel
     public IPagination Pagination { get; set; }
 
     private readonly IServiceDirectoryClient _serviceDirectoryClient;
+    private readonly IPostcodeLookup _postcodeLookup;
     private const int PageSize = 10;
 
-    public ServiceFilterModel(IServiceDirectoryClient serviceDirectoryClient)
+    public ServiceFilterModel(IServiceDirectoryClient serviceDirectoryClient, IPostcodeLookup postcodeLookup)
     {
         _serviceDirectoryClient = serviceDirectoryClient;
+        _postcodeLookup = postcodeLookup;
         Filters = FilterDefinitions.Filters;
         TypeOfSupportFilter = FilterDefinitions.CategoryFilter;
         Services = Enumerable.Empty<Service>();
@@ -59,12 +64,13 @@ public class ServiceFilterModel : PageModel
                 || longitude == null;
     }
 
-    private static void CheckParameters([NotNull] string? postcode, [NotNull] string? adminDistrict, [NotNull] float? latitude, [NotNull] float? longitude)
+    //    private static void CheckParameters([NotNull] string? postcode, [NotNull] string? adminDistrict, [NotNull] float? latitude, [NotNull] float? longitude)
+    private static void CheckParameters([NotNull] string? postcode)
     {
         ArgumentException.ThrowIfNullOrEmpty(postcode);
-        ArgumentException.ThrowIfNullOrEmpty(adminDistrict);
-        ArgumentNullException.ThrowIfNull(latitude);
-        ArgumentNullException.ThrowIfNull(longitude);
+        //ArgumentException.ThrowIfNullOrEmpty(adminDistrict);
+        //ArgumentNullException.ThrowIfNull(latitude);
+        //ArgumentNullException.ThrowIfNull(longitude);
     }
 
     private async Task<IActionResult> HandleGet(string postcode, string adminDistrict, float latitude, float longitude)
@@ -77,18 +83,30 @@ public class ServiceFilterModel : PageModel
         return Page();
     }
 
-    public Task OnPost(string? postcode, string? adminDistrict, float? latitude, float? longitude, string? remove, string? pageNum)
+    public Task<IActionResult> OnPost(string? postcode, string? adminDistrict, float? latitude, float? longitude, string? remove, string? pageNum)
     {
-        CheckParameters(postcode, adminDistrict, latitude, longitude);
+        CheckParameters(postcode); //, adminDistrict, latitude, longitude);
 
-        return HandlePost(postcode, adminDistrict, latitude.Value, longitude.Value, remove, pageNum);
+        return HandlePost(postcode, adminDistrict, latitude, longitude, remove, pageNum);
     }
 
-    private async Task HandlePost(string postcode, string adminDistrict, float latitude, float longitude, string? remove, string? pageNum)
+    private async Task<IActionResult> HandlePost(string postcode, string? adminDistrict, float? latitude, float? longitude, string? remove, string? pageNum)
     {
         IsGet = false;
 
-        Postcode = postcode;
+        if (adminDistrict == null)
+        {
+            var (postcodeError, postcodeInfo) = await _postcodeLookup.Get(postcode);
+            if (postcodeError != PostcodeError.None)
+            {
+                return RedirectToPage("/PostcodeSearch/Index", new { postcodeError });
+            }
+
+            Postcode = postcodeInfo!.Postcode;
+            adminDistrict = postcodeInfo.Codes.AdminDistrict;
+            latitude = postcodeInfo.Latitude;
+            longitude = postcodeInfo.Longitude;
+        }
 
         Filters = FilterDefinitions.Filters.Select(fd => fd.ToPostFilter(Request.Form, remove));
         TypeOfSupportFilter = FilterDefinitions.CategoryFilter.ToPostFilter(Request.Form, remove);
@@ -97,7 +115,10 @@ public class ServiceFilterModel : PageModel
         if (!string.IsNullOrWhiteSpace(pageNum))
             CurrentPage = int.Parse(pageNum);
 
-        (Services, Pagination) = await GetServicesAndPagination(adminDistrict, latitude, longitude);
+        //todo: proper checks
+        (Services, Pagination) = await GetServicesAndPagination(adminDistrict, latitude.Value, longitude.Value);
+
+        return Page();
     }
 
     private async Task<(IEnumerable<Service>, IPagination)> GetServicesAndPagination(string adminDistrict, float latitude, float longitude)
