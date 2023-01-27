095a15d5c2f88ef28c19d2875a8086f33ede2400
Phil Davies <github@phildavies.co.uk>
2023-01-27 16:02:25 +0000

    ga4 wip

--- src/FamilyHubs.ServiceDirectory.Web/scripts/components/analytics.js
+++ src/FamilyHubs.ServiceDirectory.Web/scripts/components/analytics.js
@@ -10,72 +10,90 @@ export default function loadAnalytics(gaMeasurementId) {
     function gtag() { dataLayer.push(arguments); }
     gtag('js', new Date());
 
-    // todo: disable pageview measurement and send the page_view event manually?
-    // https://developers.google.com/analytics/devguides/collection/gtagjs/pages#default_behavior
+    // disable pageview measurement and send the page_view event manually (https://developers.google.com/analytics/devguides/collection/gtagjs/pages#default_behavior)
 
     //todo: if we keep this, will have to update cookie-function
     gtag('config', gaMeasurementId, {
         send_page_view: false
     });
 
-    gtag('event',
-        'page_view',
-        {
-            page_title: document.title,
-            page_location: getPiiSafePageLocation(),
-            page_path: getPiiSafePagePath(),
-            send_to: gaMeasurementId
-        });
-
-    // overriding config defaults:
-    //gtag('config', gaMeasurementId, {
-    //    'page_view': {
-    //        'page_path': getPiiSafePagePath(),
-    //        'page_location': getPiiSafePageLocation()
-    //    }
-    //});
+    gtag('event', 'page_view', getPiiSafePageView(gaMeasurementId));
 }
 
 /*todo: if keep, these prob don't belong here*/
-/*todo: also func to get both*/
+/*todo: test*/
+/*todo: move into ts - does the pageview object have a def?*/
 
-function getPiiSafePageLocation() {
-    var location = window.location.href;
-    var urlArray = location.split("?");
-    var queryParams = new URLSearchParams(urlArray[1]);
+function getPiiSafePageView(gaMeasurementId) {
 
-    var postcode = queryParams.get("postcode");
-    if (postcode == null) {
-        return location;
-    }
-    postcode = postcode.replace(/[a-zA-Z]+$/, "");
-    queryParams.set("postcode", postcode);
-    queryParams.delete("latitude");
-    queryParams.delete("longitude");
-    // adminDistrict gets a pass
+    const pageView = {
+        page_title: document.title,
+        send_to: gaMeasurementId
+    };
 
-    var newQueryParams = queryParams.toString();
-    var newUrl = urlArray[0] + "?" + newQueryParams;
-    return newUrl;
-}
-
-function getPiiSafePagePath() {
-    const path = window.location.pathname;
     const queryString = window.location.search;
-
     const queryParams = new URLSearchParams(queryString);
 
-    var postcode = queryParams.get("postcode");
+    let postcode = queryParams.get("postcode");
     if (postcode == null) {
-        return path + queryString;
+        pageView.page_location = window.location.href;
+        pageView.page_path = window.location.pathname + queryString;
+
+        return pageView;
     }
+
     postcode = postcode.replace(/[a-zA-Z]+$/, "");
     queryParams.set("postcode", postcode);
-
     queryParams.delete("latitude");
     queryParams.delete("longitude");
-    // adminDistrict gets a pass
 
-    const newQueryParams = queryParams.toString();
-    return path + "?" + newQueryParams;
+    const newQueryParams = '?' + queryParams.toString();
+
+    const urlArray = window.location.href.split("?");
+
+    pageView.page_location = urlArray[0] + newQueryParams;
+    pageView.page_path = window.location.pathname + newQueryParams;
+
+    return pageView;
 }
+
+//function getPiiSafePageLocation() {
+//    var location = window.location.href;
+//    var urlArray = location.split("?");
+//    var queryParams = new URLSearchParams(urlArray[1]);
+
+//    var postcode = queryParams.get("postcode");
+//    if (postcode == null) {
+//        return location;
+//    }
+//    postcode = postcode.replace(/[a-zA-Z]+$/, "");
+//    queryParams.set("postcode", postcode);
+//    queryParams.delete("latitude");
+//    queryParams.delete("longitude");
+//    // adminDistrict gets a pass
+
+//    var newQueryParams = queryParams.toString();
+//    var newUrl = urlArray[0] + "?" + newQueryParams;
+//    return newUrl;
+//}
+
+//function getPiiSafePagePath() {
+//    const path = window.location.pathname;
+//    const queryString = window.location.search;
+
+//    const queryParams = new URLSearchParams(queryString);
+
+//    var postcode = queryParams.get("postcode");
+//    if (postcode == null) {
+//        return path + queryString;
+//    }
+//    postcode = postcode.replace(/[a-zA-Z]+$/, "");
+//    queryParams.set("postcode", postcode);
+
+//    queryParams.delete("latitude");
+//    queryParams.delete("longitude");
+//    // adminDistrict gets a pass
+
+//    const newQueryParams = queryParams.toString();
+//    return path + "?" + newQueryParams;
+//}
