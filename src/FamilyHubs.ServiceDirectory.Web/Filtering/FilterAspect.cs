20b9dca643d68c3b555e8c6ba75fb8206575f02e
Phil Davies <github@phildavies.co.uk>
2023-02-03 16:02:35 +0000

    wip

--- src/FamilyHubs.ServiceDirectory.Web/Filtering/FilterAspect.cs
+++ src/FamilyHubs.ServiceDirectory.Web/Filtering/FilterAspect.cs
@@ -6,16 +6,9 @@ namespace FamilyHubs.ServiceDirectory.Web.Filtering;
 //todo: more precise Id description
 //todo: put name in data- attribute for test
 //todo: allow null name and default to id for name?
-/// <param name="Id">The value that the code uses to identify the filter aspect.
-/// For sub-categories, it must be the sub-category taxonomy guid from the database.
-/// </param>
-/// <param name="Name">A friendly name. Used in the URL, so *must* be url safe and mustn't contain ','. Preferably, keep it to alphanumeric and hyphens.
-/// *Must* be unique within the category (or sub-category when using FilterSubGroups).</param>
-/// <param name="Description">As displayed in the filter panel.</param>
-/// <param name="SelectedByDefault">Is the filter auto-selected, when the user first enters the page via the post code search page?</param>
 [DebuggerDisplay("{Id}")]
 public sealed record FilterAspect(
     string Id,
-    string Name,
     string Description,
+    string? Name = null,
     bool SelectedByDefault = false) : IFilterAspect;
