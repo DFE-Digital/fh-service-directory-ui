ccbc10190c79c0a881be6736c6f38892b50b4775
Phil Davies <github@phildavies.co.uk>
2023-02-06 11:15:11 +0000

    wip

--- src/FamilyHubs.ServiceDirectory.Core/Postcode/Model/PostcodeInfo.cs
+++ src/FamilyHubs.ServiceDirectory.Core/Postcode/Model/PostcodeInfo.cs
@@ -3,16 +3,10 @@ using System.Text.Json.Serialization;
 
 namespace FamilyHubs.ServiceDirectory.Core.Postcode.Model;
 
-/// <param name="Postcode">Searched postcode in canonical format. 2, 3 or 4-character outward code, single space and 3-character inward code.</param>
-/// <param name="Latitude">The WGS84 latitude given the postcode's national grid reference. May be null if geolocation not available.</param>
-/// <param name="Longitude">The WGS84 longitude given the postcode's national grid reference. May be null if geolocation not available.</param>
-/// <param name="Outcode">The outward code is the part of the postcode before the single space in the middle. It is between two and four characters long. A few outward codes are non-geographic, not divulging where mail is to be sent. Examples of outward codes include "L1", "W1A", "RH1", "RH10" or "SE1P".</param>
-/// <param name="Codes"></param>
 public sealed record PostcodeInfo(
     [property: JsonPropertyName("postcode")] string Postcode,
     [property: JsonPropertyName("latitude")] float? Latitude,
     [property: JsonPropertyName("longitude")] float? Longitude,
-    //[property: JsonPropertyName("outcode")] string Outcode,
     [property: JsonPropertyName("codes")] Codes Codes) : IPostcodeInfo
 {
     public string? AdminArea => string.Equals(Codes.AdminCounty, "E99999999", StringComparison.InvariantCultureIgnoreCase)
