0dec87b4bf124c15530bfe1899edcc3896664e11
Phil Davies <github@phildavies.co.uk>
2023-02-20 14:04:53 +0000

    remove commented out js unit test

--- tests/FamilyHubs.ServiceDirectory.JavascriptUnitTests/Tests/AnalyticsTests.ts
+++
@@ -1,5 +0,0 @@
-﻿//import initAnalytics from '../../../src/FamilyHubs.ServiceDirectory.Web/tmp/js/components/analytics.js';
-
-//test('initAnalytics', () => {
-//    expect(initAnalytics('').toBeUndefined());
-//});
