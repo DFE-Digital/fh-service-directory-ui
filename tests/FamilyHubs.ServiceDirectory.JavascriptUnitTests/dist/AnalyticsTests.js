589353ba3ddd2c0256b62a436d7b5ad586146f2e
Phil Davies <github@phildavies.co.uk>
2023-02-20 14:05:39 +0000

    remove js output too

--- tests/FamilyHubs.ServiceDirectory.JavascriptUnitTests/dist/AnalyticsTests.js
+++
@@ -1,5 +0,0 @@
-//import initAnalytics from '../../../src/FamilyHubs.ServiceDirectory.Web/tmp/js/components/analytics.js';
-//test('initAnalytics', () => {
-//    expect(initAnalytics('').toBeUndefined());
-//});
-//# sourceMappingURL=AnalyticsTests.js.map
