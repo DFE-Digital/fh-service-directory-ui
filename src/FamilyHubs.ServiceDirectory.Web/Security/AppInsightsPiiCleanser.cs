17579a1b90ddaa7a6b93cb0dfadfd2468b5a9fa6
Phil Davies <github@phildavies.co.uk>
2023-02-27 11:27:21 +0000

    tidy up

--- src/FamilyHubs.ServiceDirectory.Web/Security/AppInsightsPiiCleanser.cs
+++ src/FamilyHubs.ServiceDirectory.Web/Security/AppInsightsPiiCleanser.cs
@@ -6,35 +6,11 @@ using System.Text.RegularExpressions;
 
 namespace FamilyHubs.ServiceDirectory.Web.Security;
 
+// note we don't redact the console log output as that doesn't get persisted anywhere
+
 //todo: doesn't belong in security
 // how much will this slow things down?
-// can we do it earlier
 // remove values/properties or the whole trace instead if it's too slow
-#if cant_modify_only_filter
-public class AppInsightsPiiCleanser : ITelemetryProcessor
-{
-    private static readonly Regex PiiRegex = new Regex(@"(?<=(postcode|latitude|longitude)=)[^&\s]+", RegexOptions.Compiled);
-
-    private static readonly string[] PropertiesToRedact =
-        new[] {"Uri", "Scope", "QueryString", "HostingRequestStartingLog"};
-
-    private ITelemetryProcessor Next { get; }
-
-    public AppInsightsPiiCleanser(ITelemetryProcessor next)
-    {
-        Next = next;
-    }
-
-    public void Process(ITelemetry item)
-    {
-        //todo: we still want to log all lines, just remove pii
-        // To filter out an item, return without calling the next processor.
-
-        //if (item.Sanitize())
-
-        //todo: we don't want to keep doing this repeatedly for all the traces in a request
-        // can we come in earlier? do the properties get passed on to the next trace?
-
         //properties
         // {[Uri, https://s181d01-as-fh-sd-api-dev.azurewebsites.net/api/services?serviceType=Family%20Experience&districtCode=E08000006&latitude=53.508884&longtitude=-2.294605&proximity=32186&pageNumber=1&pageSize=10&maxFamilyHubs=1]}
 
@@ -45,60 +21,12 @@ public class AppInsightsPiiCleanser : ITelemetryProcessor
         // {[Scope, ["HTTP GET https://api.postcodes.io/postcodes/M27%208SS"]]}
         // {[Uri, https://api.postcodes.io/postcodes/M27%208SS]}
 
-        // there's also RequestPath, but that seems to be set when there's no pii to sanitize, so Path looks a better bet
-
         //todo: check properties on exception telemetry (on filter page)
 
-        // order by least common for efficiency
-        if (item is TraceTelemetry traceTelemetry
-            && traceTelemetry.Properties.TryGetValue("Path", out string? path)
-            && path is "/ServiceFilter"
-            && traceTelemetry.Properties.TryGetValue("Method", out string? method)
-            && method is "GET")
-        {
-            traceTelemetry.Message = Sanitize(traceTelemetry.Message);
-            foreach (string propertyKey in PropertiesToRedact)
-            {
-                SanitizeProperty(traceTelemetry.Properties, propertyKey);
-            }
-        }
-
-        //if (!OKtoSend(item))
-        //{
-        //    return;
-        //}
-
-        Next.Process(item);
-    }
-
-    private void SanitizeProperty(IDictionary<string,string> properties, string key)
-    {
-        if (properties.TryGetValue(key, out string? value))
-        {
-            properties[key] = Sanitize(value);
-        }
-    }
 
     // regex to handle all, or different methods for each?
     // Request starting HTTP/2 GET https://localhost:7199/ServiceFilter?postcode=M27%208SS&adminarea=E08000006&latitude=53.508884&longitude=-2.294605&frompostcodesearch=True - -
     // ?postcode=M27%208SS&adminarea=E08000006&latitude=53.508884&longitude=-2.294605&frompostcodesearch=True
-    private string Sanitize(string value)
-    {
-        return PiiRegex.Replace(value, "REDACTED");
-        //return $"Test-{value}";
-    }
-
-//#pragma warning disable
-//    private bool OKtoSend(ITelemetry item)
-//    {
-//        //var dependency = item as DependencyTelemetry;
-//        //if (dependency == null) return true;
-
-//        //return dependency.Success != true;
-//        return true;
-//    }
-}
-#endif
 
 /// <remarks>
 /// See https://learn.microsoft.com/en-us/azure/azure-monitor/app/api-filtering-sampling
@@ -287,20 +215,3 @@ public class RedactPiiInitializer : ITelemetryInitializer
         }
     }
 }
-
-// do we also need to redact from serilog??
-
-/*
- *Yes, if you redact sensitive information from what gets sent to Application Insights, you should also remove the sensitive data from Serilog logs as well. This is because Serilog can also send logs to Application Insights, and if the sensitive data is included in the Serilog logs, it can still end up in Application Insights.
-
-To ensure that sensitive data is not included in your logs, you can use Serilog's filtering capabilities to remove or replace sensitive data before it is logged. For example, you can use the WithSensitiveData() method to specify properties that should be treated as sensitive, and then use the Destructure.ByTransforming() method to redact those properties:
-
-csharp
-Copy code
-Log.Logger = new LoggerConfiguration()
-    .Destructure.ByTransforming<MySensitiveData>(x => new MySensitiveData { Password = "****" }) // replace password with "****"
-    .WriteTo.ApplicationInsights(...)
-    .CreateLogger();
-In this example, MySensitiveData is a custom class that contains sensitive information (in this case, a Password property). The Destructure.ByTransforming() method is used to transform instances of MySensitiveData by creating a new instance with the sensitive information redacted (replaced with "****"). This ensures that the redacted data is not included in the logs that are sent to Application Insights.
- *
- */
