a8d3871b10a4a8c34f70b8f5411aa019d4aca6d3
Phil Davies <github@phildavies.co.uk>
2023-02-28 14:11:45 +0000

    unit tests wip

--- tests/FamilyHubs.ServiceDirectory.UnitTests/Web/Telemetry/TelemetryPiiRedactorTests.cs
+++ tests/FamilyHubs.ServiceDirectory.UnitTests/Web/Telemetry/TelemetryPiiRedactorTests.cs
@@ -16,8 +16,35 @@ namespace FamilyHubs.ServiceDirectory.UnitTests.Web.Telemetry
         }
 
         [Theory]
-        [InlineData("https://localhost:7199/ServiceFilter?postcode=REDACTED&adminarea=E08000006&latitude=REDACTED&longitude=REDACTED&frompostcodesearch=True", "https://localhost:7199/ServiceFilter?postcode=M27 8SS&adminarea=E08000006&latitude=53.508884&longitude=-2.294605&frompostcodesearch=True")]
+        // actual url
+        [InlineData("https://find-support-for-your-family.education.gov.uk/ServiceFilter?postcode=REDACTED&adminarea=E08000006&latitude=REDACTED&longitude=REDACTED&frompostcodesearch=True", "https://find-support-for-your-family.education.gov.uk/ServiceFilter?postcode=M27 8SS&adminarea=E08000006&latitude=53.508884&longitude=-2.294605&frompostcodesearch=True")]
+        // redactable parameter at the end (in case url changes)
+        [InlineData("https://find-support-for-your-family.education.gov.uk/ServiceFilter?frompostcodesearch=True&postcode=REDACTED&adminarea=E08000006&latitude=REDACTED&longitude=REDACTED", "https://find-support-for-your-family.education.gov.uk/ServiceFilter?frompostcodesearch=True&postcode=M27 8SS&adminarea=E08000006&latitude=53.508884&longitude=-2.294605")]
         public void RequestTelemetry_RedactPiiTest(string expectedUrl, string initialUrl)
+        {
+            TestRequestTelemetry(expectedUrl, initialUrl);
+        }
+
+        [Theory]
+        [InlineData("M1 1AA")]
+        [InlineData("M60 1NW")]
+        [InlineData("CR2 6XH")]
+        [InlineData("DN55 1PT")]
+        [InlineData("W1P 1HQ")]
+        [InlineData("EC1A 1BB")]
+        // we use the canonical postcode format in the url, but check non-canonical formats just in case there's a later change
+        [InlineData("M11AA")]
+        [InlineData("M601NW")]
+        [InlineData("CR26XH")]
+        [InlineData("DN551PT")]
+        [InlineData("W1P1HQ")]
+        [InlineData("EC1A1BB")]
+        public void RequestTelemetry_PostcodeFormats_RedactPiiTest(string postcode)
+        {
+            TestRequestTelemetry("https://find-support-for-your-family.education.gov.uk/ServiceFilter?postcode=REDACTED&adminarea=E08000006&latitude=REDACTED&longitude=REDACTED&frompostcodesearch=True", $"https://find-support-for-your-family.education.gov.uk/ServiceFilter?postcode={postcode}&adminarea=E08000006&latitude=53.508884&longitude=-2.294605&frompostcodesearch=True");
+        }
+
+        private void TestRequestTelemetry(string expectedUrl, string initialUrl)
         {
             var requestTelemetry = CreateRequestTelemetry("GET /ServiceFilter/Index", initialUrl);
             TelemetryPiiRedactor.Initialize(requestTelemetry);
