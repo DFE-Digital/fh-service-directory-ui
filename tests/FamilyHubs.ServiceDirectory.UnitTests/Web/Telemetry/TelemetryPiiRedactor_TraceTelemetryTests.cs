5d3616705215cb2e2bd3e5f15cf9c2307307dd29
Phil Davies <github@phildavies.co.uk>
2023-03-02 13:37:02 +0000

    unit tests wip

--- tests/FamilyHubs.ServiceDirectory.UnitTests/Web/Telemetry/TelemetryPiiRedactor_TraceTelemetryTests.cs
+++ tests/FamilyHubs.ServiceDirectory.UnitTests/Web/Telemetry/TelemetryPiiRedactor_TraceTelemetryTests.cs
@@ -14,8 +14,11 @@ public class TelemetryPiiRedactor_TraceTelemetryTests
     }
 
     [Theory]
+    // redacting
     [InlineData("Start processing HTTP request GET https://api.postcodes.io/postcodes/REDACTED", "Start processing HTTP request GET https://api.postcodes.io/postcodes/M27%208SS")]
     [InlineData("Request starting HTTP/2 GET https://find-support-for-your-family.education.gov.uk/ServiceFilter?postcode=REDACTED&adminarea=E08000006&latitude=REDACTED&longitude=REDACTED&frompostcodesearch=True - ", "Request starting HTTP/2 GET https://find-support-for-your-family.education.gov.uk/ServiceFilter?postcode=M27%206NF&adminarea=E08000006&latitude=53.53087&longitude=-2.349673&frompostcodesearch=True - ")]
+    // non redacting
+    [InlineData("Request starting HTTP/2 POST https://find-support-for-your-family.education.gov.uk/ServiceFilter?_gl=1*rwc3vy*_up*MQ..*_ga*MzkwODc1NzA2LjE2Nzc3NjM5NzI.*_ga_TD99KTZEE1*MTG3Nzc1Mzk3MS4xLjAuMTY3Nzc2NDAzMi4wLjAuMA.. application/x-www-form-urlencoded 199", "Request starting HTTP/2 POST https://find-support-for-your-family.education.gov.uk/ServiceFilter?_gl=1*rwc3vy*_up*MQ..*_ga*MzkwODc1NzA2LjE2Nzc3NjM5NzI.*_ga_TD99KTZEE1*MTG3Nzc1Mzk3MS4xLjAuMTY3Nzc2NDAzMi4wLjAuMA.. application/x-www-form-urlencoded 199")]
     public void RedactPiiFromMessageTest(string expectedMessage, string message)
     {
         TestTraceTelemetry(expectedMessage, message);
