65d9fbf52ed00b64f6a7ce8f7b5d5e17afb5caf5
Hardik Desai <hardik.desai@education.gov.uk>
2023-04-20 11:50:50 +0100

    order Taxonomies By ParentId ThenBy Id

--- src/FamilyHubs.ServiceDirectory.Web/Mappers/ServiceMapper.cs
+++ src/FamilyHubs.ServiceDirectory.Web/Mappers/ServiceMapper.cs
@@ -66,7 +66,7 @@ public static class ServiceMapper
 
     private static IEnumerable<string> GetCategories(ServiceDto service)
     {
-        return  service.Taxonomies.OrderBy(st => st.Id).Select(st => st.Name);
+        return  service.Taxonomies.OrderBy(st => st.ParentId).ThenBy(st => st.Id).Select(st => st.Name);
     }
 
     private static IEnumerable<string> GetAddress(LocationDto location)
