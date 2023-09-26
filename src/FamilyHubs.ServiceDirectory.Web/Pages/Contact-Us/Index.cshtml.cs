2059bf62a01272724fe2c4d3a980d8f8dd216259
Phil Davies <github@phildavies.co.uk>
2023-09-26 11:33:14 +0100

    use component contact us

--- src/FamilyHubs.ServiceDirectory.Web/Pages/Contact-Us/Index.cshtml.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace FamilyHubs.ServiceDirectory.Web.Pages.Contact_Us;
-
-public class IndexModel : PageModel
-{
-}
---
+++ src/FamilyHubs.ServiceDirectory.Web/Pages/Shared/FamilyHubsUiPageModel.cs
@@ -0,0 +1,15 @@
+﻿using FamilyHubs.SharedKernel.Razor.FamilyHubsUi.Options;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
+
+namespace FamilyHubs.ServiceDirectory.Web.Pages.Shared;
+
+public class FamilyHubsUiPageModel : PageModel
+{
+    public IFamilyHubsUiOptions FamilyHubsUiOptions { get; }
+
+    public FamilyHubsUiPageModel(IOptions<FamilyHubsUiOptions> familyHubsUiOptions)
+    {
+        FamilyHubsUiOptions = familyHubsUiOptions.Value;
+    }
+}
